18a44b05b7732db9a7df8227158452f95909b3bb
Eddie O'Neill <ejosephoneill@gmail.com>
2019-11-27 21:33:27 -0500

    initial commit for RoadTripGame

---
+++ RoadTripGame/RoadTripGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoadTripGame
+//
+//  Created by Edward O'Neill on 11/27/19.
+//  Copyright © 2019 Edward O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

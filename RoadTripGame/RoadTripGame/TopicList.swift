a6a0daf29cf638e097539525845f77668e7df37f
Eddie O'Neill <ejosephoneill@gmail.com>
2019-11-28 01:25:10 -0500

    added background image

--- RoadTripGame/RoadTripGame/TopicList.swift
+++ RoadTripGame/RoadTripGame/TopicList.swift
@@ -6,16 +6,77 @@
 //  Copyright © 2019 Edward O'Neill. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class TopicList {
-    var topic = [""]
+    var topic: Set<String> = []
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let carList = ["Find yellow car", "Find Tesla", "Find Bus"]
+    let name = ["Find City name"]
+    let sign = ["Find speed sign", "Find direction sign", "Find exit sign"]
+    let animals = ["Find any animal"]
     
     func randomTopicList() -> [String] {
-        for num in 1...40 {
-            self.topic.append("\(num)")
+        var randomArr = [String]()
+        
+        for char in alphabet.uppercased() {
+            self.topic.insert("Word that starts with \(char)")
         }
         
-        return Array(Set(topic))
+        for num in 0...9 {
+            topic.insert("Find number: \(num)")
+        }
+        
+        for word in carList{
+            topic.insert(word)
+        }
+        
+        for word in name {
+            topic.insert(word)
+        }
+        
+        for word in sign {
+            topic.insert(word)
+        }
+        
+        for word in animals {
+            topic.insert(word)
+        }
+        
+        for word in topic {
+            randomArr.append(word)
+        }
+        
+        return randomArr
+    }
+    
+    func imageSorter() -> [String: String] {
+        var imageDict = [String: String]()
+
+        for char in alphabet.uppercased() {
+            imageDict["Word that starts with \(char)"] = "word"
+        }
+        
+        for num in 0...9 {
+            imageDict["Find number: \(num)"] = "number"
+        }
+        
+        for word in carList {
+            imageDict[word] = "car"
+        }
+        
+        for city in name {
+            imageDict[city] = "city"
+        }
+        
+        for sign in sign {
+            imageDict[sign] = "sign"
+        }
+        
+        for animal in animals {
+            imageDict[animal] = "animal"
+        }
+        
+        return imageDict
     }
 }
--- RoadTripGame/RoadTripGame/ViewController.swift
+++ RoadTripGame/RoadTripGame/ViewController.swift
@@ -14,10 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var redScore: UILabel!
     @IBOutlet weak var blueScore: UILabel!
     @IBOutlet var winner: [UIButton]!
+    @IBOutlet weak var pic: UIImageView!
     
     var score0 = 0
     var score1 = 0
     var topicList = TopicList().randomTopicList()
+    let imageName = TopicList().imageSorter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,34 @@ class ViewController: UIViewController {
     }
     
     func newTopic() {
-        let topics = topicList.popLast()
+        let topics = topicList.popLast() ?? ""
+        print(topics)
         topic.text = topics
+        
+        if imageName[topics] == "car" {
+            pic.image = #imageLiteral(resourceName: "carBackground")
+            topic.textColor = .black
+        } else if imageName[topics] == "city" {
+            pic.image = #imageLiteral(resourceName: "1127620")
+            topic.textColor = .black
+        } else if imageName[topics] == "word" {
+            pic.image = #imageLiteral(resourceName: "wordsBackground")
+            topic.textColor = .black
+        } else if imageName[topics] == "animal" {
+            pic.image = #imageLiteral(resourceName: "animalBackGround")
+            topic.textColor = .black
+        } else if imageName[topics] == "sign" {
+            pic.image = #imageLiteral(resourceName: "EH8Y44")
+            topic.textColor = .black
+        } else if imageName[topics] == "number" {
+            pic.image = #imageLiteral(resourceName: "2635138")
+            topic.textColor = .black
+        } else if topics == "" {
+            topicList = TopicList().randomTopicList()
+            newTopic()
+        }
+        
+        
     }
     
     func reset() {
@@ -38,6 +66,7 @@ class ViewController: UIViewController {
         score0 = 0
         blueScore.text = "Team Blue \(score1)"
         redScore.text = "Team Red: \(score0)"
+        pic.backgroundColor = .white
         for button in winner {
             button.isEnabled = true
         }
@@ -61,13 +90,23 @@ class ViewController: UIViewController {
         
         if score1 == 20 {
             topic.text = "Blue Wins"
+            pic.backgroundColor = .blue
+            pic.image = nil
+            topic.textColor = .white
             disableButton()
         } else if score0 == 20 {
             topic.text = "Red Wins"
+            pic.backgroundColor = .red
+            pic.image = nil
+            topic.textColor = .white
             disableButton()
         } else {
             newTopic()
         }
+    }
+    
+    @IBAction func skip(_ sender: UIButton) {
+        newTopic()
     }
     
     @IBAction func reset(_ sender: UIButton) {
